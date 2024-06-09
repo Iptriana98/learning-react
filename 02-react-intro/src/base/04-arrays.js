e4c64d280253a4aa703c638e874907bd3c7db3c8
Ibrahin Perez <iptrianaa@gmail.com>
2024-06-09 13:30:19 -0500

    added arrays file

---
+++ 02-react-intro/src/base/04-arrays.js
@@ -0,0 +1,8 @@
+// Arrays in js
+const array = new Array(100); // 100 elements empty
+const array1 = [1, 2, 3, 4, 5]; // 5 elements
+const array2 = [...array1, 6] // create a copy of array1 and add '6' at the end
+
+
+
+console.log(array2)
