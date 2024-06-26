f6a0e09e58be06afa49dc607a6cba469bf9d326e
Ibrahin Perez <iptrianaa@gmail.com>
2024-06-26 04:31:27 -0500

    using ract props

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,7 +1,13 @@
-export const FirstApp = () => {
+export const FirstApp = ({title, subtitle}) => {
     return (
         <div>
-            <h1>FirstApp !!!</h1>
+            <h1>{title}</h1>
+            <p>{subtitle}</p>
         </div>
     )
+}
+
+FirstApp.propTypes = {
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string
 }
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -2,18 +2,8 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { FirstApp } from "./FirstApp";
 
-// function App() {
-//     const name = "Goku";
-//     return (
-//         <div>
-//             <h1>Hello World</h1>
-//             <h1>I'm {name}</h1>
-//         </div>
-//     );
-// }
-
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <FirstApp/>
+        <FirstApp title="Counter App" subtitle="This is a subtitle"/>
     </React.StrictMode>
 )
