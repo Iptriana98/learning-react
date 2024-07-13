638d10fa992252879f2d7e8e859535fcfce3264f
Ibrahin Perez <iptrianaa@gmail.com>
2024-07-13 05:39:12 -0500

    show a list of items in main

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,7 +1,15 @@
 export const GifExpertApp = () => {
+    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball'])
     return (
         <>
             <h1>GifExpertApp</h1>
+            <ol>
+                {
+                    categories.map(category => {
+                        return <li key={category}>{category}</li>
+                    })
+                }
+            </ol>
         </>
     )
 }
