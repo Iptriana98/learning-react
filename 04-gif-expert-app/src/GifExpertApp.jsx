73b0f7340045dae13942d61fc0888c0274d1fb60
Ibrahin Perez <iptrianaa@gmail.com>
2024-07-13 06:12:43 -0500

    added functional add button

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,8 +1,14 @@
+import { useState } from 'react'
 export const GifExpertApp = () => {
     const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball'])
+
+    const handleAdd = () => {
+        setCategories([...categories, 'HunterXHunter'])
+    }
     return (
         <>
             <h1>GifExpertApp</h1>
+            <button onClick={handleAdd}>Add</button>
             <ol>
                 {
                     categories.map(category => {
