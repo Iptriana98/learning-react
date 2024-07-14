5b506dedd1c03147b6349946f160e9ae66f8f8dd
Ibrahin Perez <iptrianaa@gmail.com>
2024-07-14 15:49:14 -0500

    added form functionality in AddCategory component

--- 04-gif-expert-app/src/components/AddCategory.jsx
+++ 04-gif-expert-app/src/components/AddCategory.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { useState } from 'react'
 
-export const AddCategory = () => {
-  return (
-    <input
-      type="text"
-      placeholder='Add Category'
-      />
-  )
+export const AddCategory = ({ onAdd }) => {
+    const [inputValue, setInputValue] = useState('')
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+        onAdd( categories => [...categories, inputValue])
+        if (inputValue.trim().length <= 1) return
+        setInputValue('')
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <input
+                type="text"
+                placeholder='Add Category'
+                value={inputValue}
+                onChange={event => setInputValue(event.target.value)}
+            />
+
+        </form>
+    )
 }
