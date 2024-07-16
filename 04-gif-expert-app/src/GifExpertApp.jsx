b0b762352802b34b31bdbe9c06cb2d0171f5ec8c
Ibrahin Perez <iptrianaa@gmail.com>
2024-07-15 20:41:03 -0500

    changed data flow direction

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -3,14 +3,14 @@ import { AddCategory } from './components/AddCategory'
 export const GifExpertApp = () => {
     const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball'])
 
-    const handleAdd = () => {
-        setCategories([...categories, 'HunterXHunter'])
+    const handleAdd = (newCategory) => {
+        setCategories([...categories, newCategory])
     }
     return (
         <>
             <h1>GifExpertApp</h1>
-            <AddCategory onAdd={setCategories}/>
-            <button onClick={handleAdd}>Add</button>
+            <AddCategory onAdd={handleAdd}/>
+            {/* <button onClick={handleAdd}>Add</button> */}
             <ol>
                 {
                     categories.map(category => {
