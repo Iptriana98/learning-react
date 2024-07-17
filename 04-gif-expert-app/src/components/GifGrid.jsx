1c6bc8eecbef037d527a6c1417781c88e4875dae
Ibrahin Perez <iptrianaa@gmail.com>
2024-07-17 05:13:50 -0500

    showed response in the screen

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
 import { getGifs } from '../helpers/getGifs'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export const GifGrid = ({ category }) => {
-    
-    useEffect(() => {
-      getGifs(category)
 
+    const [images, setImages] = useState([]);
+
+    const getImages = async () => {
+        const newImages = await getGifs(category)
+        setImages(newImages)
+    }
+
+    useEffect(() => {
+        getImages()
     }, [])
-    
+
     return (
-        <div>{category}</div>
+        <>
+            <h3>{category}</h3>
+            <ol>
+                {
+                    images.map(image => (
+                        <li key={image.id}>
+                            {/* <img src={image.url} alt={image.title} /> */}
+                            <p>{image.title}</p>
+                        </li>
+                    ))
+                }
+            </ol>
+        </>
     )
 }
